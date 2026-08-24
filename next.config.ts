43476add4a2c5febf2a9f4bdba39945465844161
maxmurr <maxmurr.m@gmail.com>
2026-08-24 22:25:24 +0700

    Configure chat as default route

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/chat",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
